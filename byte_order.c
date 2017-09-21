b0a25adc5a68223621250b89d03cd60261cad054
rictr0y <richard2104a@gmail.com>
2017-09-21 05:19:37 -0700

    with binary

--- byte_order.c
+++ byte_order.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
 int main() {
-  uint8_t buf[] = { 0x00, 0x50 };
+  uint8_t buf[] = { 0x01, 0x50 }; // Little Endian: 0x5001, Big Endian: 0x0150
+  uint16_t host_byte = 0x3412; // 0x3412
+
   uint16_t* port = (uint16_t*)buf;
-  printf("port number is 0x%x\n", *port);
+  
+  printf( "%x to %x\n", host_byte, ntohs(host_byte));
+  printf("port number is 0x%x\n", *port); //0x5000
+  printf("(function) port number: 0x%x\n", ntohs(*port)); //0x5000
   return 0;
 }
 
